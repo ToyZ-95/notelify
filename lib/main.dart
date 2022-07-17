507148e70e612437ad228ab53c0d836b2f894f45
Asad <patelasad4@gmail.com>
2022-07-17 08:39:15 +0530

    assets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:notelify/config/custom_theme.dart';
 import 'package:notelify/controllers/user_controller.dart';
+import 'package:notelify/views/dashboard/dashboard.dart';
 import 'package:notelify/views/login_signup/login.dart';
+
+bool goToHomePage = false;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   Get.put(UserController());
+
+  if (FirebaseAuth.instance.currentUser != null &&
+      FirebaseAuth.instance.currentUser!.emailVerified) {
+    goToHomePage = true;
+  }
+
   runApp(const MyApp());
 }
 
@@ -25,7 +35,7 @@ class MyApp extends StatelessWidget {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
           theme: CustomTheme.lightTheme,
-          home: Login(),
+          home: goToHomePage ? const Dashboard() : Login(),
         );
       },
     );
